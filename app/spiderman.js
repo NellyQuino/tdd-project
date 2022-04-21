28805830bba461928d8cd144ff494db8653db24d
NellyQuino <71736979+NellyQuino@users.noreply.github.com>
2022-04-20 22:54:12 -0700

    create class Spiderman

---
+++ app/spiderman.js
@@ -0,0 +1,11 @@
+class Spiderman {
+    constructor(name, studio, age, actor, movies) {
+        this.name = name
+        this.studio = studio
+        this.age = age
+        this.actor = actor
+        this.movies = movies
+    }
+}
+
+module.exports = Spiderman;
--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,14 +1,15 @@
+const Spiderman = require('./../app/spiderman')
+
 describe("Unit Test for Spiderman class", () => {
     test('1) Create an Spiderman object', () => {
 
-        const andrewGarfiel = new Spidermna("Spiderman Sony", 31, "Andrew Garfiel", 2, "Sony")
-        expect(resultOfSomething).toBe(18);
+        const andrewGarfiel = new Spiderman("Spiderman Sony", 31, "Andrew Garfiel", 2, "Sony")
 
         //Validamos que este codigo funciones de forma esperada 
         expect(andrewGarfiel.name).toBe("Spiderman Sony")
         expect(andrewGarfiel.age).toBe(31)
         expect(andrewGarfiel.actor).toBe("Andrew Garfiel")
-        expect(andrewGarfiel.movies).toBe(31)
+        expect(andrewGarfiel.movies).toBe(2)
         expect(andrewGarfiel.studio).toBe("Sony")
     });
 })
