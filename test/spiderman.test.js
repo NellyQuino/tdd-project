92363fc9d8d7f07d6db7de5598eed80391ba5ae8
NellyQuino <71736979+NellyQuino@users.noreply.github.com>
2022-04-20 22:35:14 -0700

    create TDD

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,14 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-        const resultOfSomething = 1 + 2
+describe("Unit Test for Spiderman class", () => {
+    test('1) Create an Spiderman object', () => {
+
+        const andrewGarfiel = new Spidermna("Spiderman Sony", 31, "Andrew Garfiel", 2, "Sony")
         expect(resultOfSomething).toBe(18);
+
+        //Validamos que este codigo funciones de forma esperada 
+        expect(andrewGarfiel.name).toBe("Spiderman Sony")
+        expect(andrewGarfiel.age).toBe(31)
+        expect(andrewGarfiel.actor).toBe("Andrew Garfiel")
+        expect(andrewGarfiel.movies).toBe(31)
+        expect(andrewGarfiel.studio).toBe("Sony")
     });
 })
