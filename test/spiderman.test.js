ae34279855709459d8429c9b1c4326a88aab01bb
NellyQuino <71736979+NellyQuino@users.noreply.github.com>
2022-04-20 23:04:26 -0700

    add second TDD

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -12,4 +12,9 @@ describe("Unit Test for Spiderman class", () => {
         expect(andrewGarfiel.movies).toBe(2)
         expect(andrewGarfiel.studio).toBe("Sony")
     });
+    test('2) Use the method getInfo()', () => {
+        const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
+
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel Studio")
+    })
 })
