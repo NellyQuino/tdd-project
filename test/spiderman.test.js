1dc007b467913b0ac3b8dddbb55d915a91f20eab
NellyQuino <71736979+NellyQuino@users.noreply.github.com>
2022-04-20 23:12:43 -0700

    add getInfom method

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -15,6 +15,6 @@ describe("Unit Test for Spiderman class", () => {
     test('2) Use the method getInfo()', () => {
         const tomHolland = new Spiderman("Spiderman Marvel", 25, "Tom Holland", 5, "Marvel")
 
-        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel Studio")
+        expect(tomHolland.getInfo()).toBe("Hey, I'm Tom Holland from Marvel studio")
     })
 })
